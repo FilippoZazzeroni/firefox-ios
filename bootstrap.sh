e9b477427445c859b4636d928b5fd4f4ae965bf2
Filippo <fzazzeroni@mozilla.com>
2024-12-19 09:57:55 +0100

    ciao

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,30 +6,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #
 # Use the --force option to force a re-build locales.
-# Use the --importLocales option to fetch and update locales only
-
-getLocale() {
-  echo "Getting locale..."
-  git clone "https://github.com/FilippoZazzeroni/LocalizationTools.git" || exit 1
-}
 
 if [ "$1" == "--force" ]; then
-    rm -rf firefoxios-l10n
-    rm -rf LocalizationTools
     rm -rf build
-fi
-
-if [ "$1" == "--importLocales" ]; then
-  # Import locales
-  if [ -d "/firefoxios-l10n" ] && [ -d "/LocalizationTools" ]; then
-      echo "l10n directories found. Not downloading scripts."
-  else
-      echo "l10n directory not found. Downloading repo and scripts."
-      getLocale
-  fi
-
-  ./firefox-ios/import-strings.sh
-  exit 0
 fi
 
 # Download the nimbus-fml.sh script from application-services.
