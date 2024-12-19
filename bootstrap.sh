e8f38c73fdf8e595daa3b6be9717e60ae334e813
FilippoZazzeroni <55580351+FilippoZazzeroni@users.noreply.github.com>
2024-12-19 09:47:55 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@
 getLocale() {
   echo "Getting locale..."
   rm -rf LocalizationTools
-  git clone https://github.com/mozilla-mobile/LocalizationTools.git || exit 1
+  git clone https://github.com/FilippoZazzeroni/LocalizationTools.git || exit 1
 }
 
 if [ "$1" == "--force" ]; then
